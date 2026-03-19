e2c2e3d28be6f60dc29722cd6bc060306c6d2835
Giovani Farias <riccog.25@gmail.com>
2026-03-19 00:03:30 -0300

    feat: add init command

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use clap::{Parser, Subcommand};
+use std::fs;
+use std::path::Path;
+
+#[derive(Parser)]
+#[command(name = "rgit")]
+#[command(about = "A tiny Git implementation in Rust", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    HashObject {
+        file: String,
+        #[arg(short, long)]
+        write: bool,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => {
+            match init_repo() {
+                Ok(_) => println!("Initialized empty rgit repository in .rgit/"),
+                Err(e) => eprintln!("Error: {}", e),
+            }
+        }
+
+       Commands::HashObject { file, write } => {
+            let path = Path::new(file);
+            if !path.exists() {
+                eprintln!("File not found: {}", file);
+                return;
+            }
+
+            let content = fs::read(path).expect("Could not read file");
+
+            if *write {
+                let repo_root = Path::new(".");
+                match rgit::objects::store_blob(repo_root, &content) {
+                    Ok(hash) => println!("{}", hash.as_str()),
+                    Err(e) => eprintln!("Error storing blob: {}", e),
+                }
+            } else {
+                let hash = rgit::objects::hash_blob(&content);
+                println!("{}", hash);
+            }
+        }
+    }
+}
+
+fn init_repo() -> std::io::Result<()> {
+    let base_dir = Path::new(".rgit");
+
+    if !base_dir.exists() {
+        fs::create_dir(base_dir)?;
+    }
+
+    fs::create_dir_all(base_dir.join("objects"))?;
+    fs::create_dir_all(base_dir.join("refs/heads"))?;
+
+    let head_path = base_dir.join("HEAD");
+    fs::write(head_path, "ref: refs/heads/main\n")?;
+
+    Ok(())
+}
